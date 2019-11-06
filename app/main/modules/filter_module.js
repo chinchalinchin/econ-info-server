745d52a1492d2d2d883f10bd37920a4129a09dcc
Grant Moore <grant.moore@ibm.com>
2019-11-06 15:31:14 -0500

    update filter module for formatting FRED dataset output. added more html templating to economic-item

--- app/main/modules/filter_module.js
+++ app/main/modules/filter_module.js
@@ -1,7 +1,13 @@
 var main_module = angular.module('finneas_stockholm')
 
-main_module.filter('GDP', function(){
-    return function(input){
-        return `${input} billion per year`
-    };
-})
+main_module
+    .filter('billionPerYear', function(){
+        return function(input){
+            return `${input} billion per year`
+        };
+    })
+    .filter('moneyVelocity', function(){
+        return function(input){
+            return `${input} turnover rate per $1 GDP`
+        }
+    })
